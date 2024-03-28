fe9e4c8e63785f4d6c30aa8ee0f6289ab90d2bea
Akihisa Yamaguchi <71268501+albasiempre@users.noreply.github.com>
2024-03-28 14:36:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.listen(PORT, () => console.log("サーバーが起動しました"));
 // Corsの設定
 
 app.use(cors({
-  origin: 'http://localhost:3000', //アクセス許可するオリジン
+  origin: 'www.albasiempre.com', //アクセス許可するオリジン
   credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
   optionsSuccessStatus: 200 //レスポンスstatusを200に設定
 }))
